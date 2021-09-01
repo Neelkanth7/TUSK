c818d1aabd712f39ecfea61c1dbd3bf264d6e548
Het <het.thakkar809@gmail.com>
2021-09-01 00:17:17 -0400

    build: added setup file for package installation

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+  name='tusk',
+  version=0.1,
+  description='This is a test package to demonstrate a good repository',
+  packages=['tusk'],
+  license='MIT',
+  install_requires=[
+    'six'
+  ]
+)
